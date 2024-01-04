82aa9476b1e8ef8c088f269d3dfe7bdd36b796f4
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-04 16:48:27 +0530

    Create Datatypes.py

---
+++ Datatypes.py
@@ -0,0 +1,16 @@
+# string type data
+book = "Hello World"
+print(type(book))
+# number type data
+num1 = 45
+print(type(num1))
+num2 = 34.9
+print(type(num2))
+
+
+'''
+  ========================OUTPUT SECTION==========================
+  <class 'str'>
+  <class 'int'>
+  <class 'float'>
+'''
