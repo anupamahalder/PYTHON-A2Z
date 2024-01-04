6e92c87bc418bd9fc85f6085ee163fd59bdf5d20
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-04 16:10:06 +0530

    Update Hello world.py

--- Hello world.py
+++ Hello world.py
@@ -1,3 +1,18 @@
 print("Hello world")
 print('I am Anupama')
 print("'Hello man'")
+print(123)
+
+
+
+
+
+
+
+'''
+========================OUTPUT SECTION========================
+Hello world
+I am Anupama
+'Hello man'
+123
+'''
