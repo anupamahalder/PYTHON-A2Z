e28bc200038884892738827d00bac867b62ac26c
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-07 23:21:57 +0530

    Create Assignment_Operators.cpp

---
+++ Assignment_Operators.cpp
@@ -0,0 +1,35 @@
+# Assignment Operators
+d = 7
+d += 5
+print(d) #12
+
+d -= 2
+print(d) #10
+
+d *= 3
+print(d) #30
+
+d /= 2
+print(d) #15.0
+
+d %= 10
+print(d) #5.0
+
+d //= 2
+print(d) #2.0
+
+d **= 3
+print(d) #8
+#-----------------------#
+k = 5
+k |= 2
+print(k) #7
+
+k ^= 3
+print(k) #4
+
+k >>= 2
+print(k) #1
+
+k <<= 4
+print(k) #16
