4ac5d021f7e7696cd69bc7e246538e59852c8c56
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-07 21:56:55 +0530

    Update DataTypes.py

--- DataTypes.py
+++ DataTypes.py
@@ -20,6 +20,7 @@ Bool = True
 print(Bool)
 print(type(Bool))
 print(7==9) #False
+
 # ---------------Binary types----------------------------
 #-----------bytes()--------------
 myList = [1,4,5,34,55,122]
@@ -34,10 +35,35 @@ b1 = bytearray(yourList)
 print(type(b1)) #<class 'bytearray'>
 # we can modify the bytearray 
 b1[0] = 9 # X
+
 # ---------------None type-------------------------------
 x = None
 print(x) #None
 print(type(x)) #<class 'NoneType'>
+
+# ---------------Sequence type----------------------
+# --------list type data----------
+li = ["Tom","Ram","Piya","Uma"]
+print(li) #['Tom', 'Ram', 'Piya', 'Uma']
+print(type(li)) #<class 'list'>
+# list data can be modifiable
+li[1] = "Rom"
+print(li) #['Tom', 'Rom', 'Piya', 'Uma']
+
+# --------tuple type data-----------
+tup = (4, 5, 6, 3)
+print(tup) #(4, 5, 6, 3)
+print(type(tup)) #<class 'tuple'>
+# tuple data cannot be changed 
+# tup[1] = 100 # X
+
+# ----------Range type data---------
+ran = range(6)
+print(ran) #range(0, 6)
+print(type(ran)) #<class 'range'>
+for i in ran:
+    print(i) # 0 1 2 3 4 5 
+
 
 
 '''
