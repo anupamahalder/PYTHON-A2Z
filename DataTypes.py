4564a9f12975979be4189720024cfb0b479beb0c
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-07 21:03:14 +0530

    Update and rename Datatypes.py to DataTypes.py

--- DataTypes.py
+++ DataTypes.py
@@ -20,6 +20,25 @@ Bool = True
 print(Bool)
 print(type(Bool))
 print(7==9) #False
+# ---------------Binary types----------------------------
+#-----------bytes()--------------
+myList = [1,4,5,34,55,122]
+b = bytes(myList)
+print(type(b)) #<class 'bytes'>
+# we cannot modify the bytes object 
+# b[0] = 9 # X
+# ----------bytearray()----------
+# binary type data
+yourList = [14,76,5,34,55,122]
+b1 = bytearray(yourList)
+print(type(b1)) #<class 'bytearray'>
+# we can modify the bytearray 
+b1[0] = 9 # X
+# ---------------None type-------------------------------
+x = None
+print(x) #None
+print(type(x)) #<class 'NoneType'>
+
 
 '''
   ========================OUTPUT SECTION==========================
