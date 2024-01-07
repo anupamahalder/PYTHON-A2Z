5af5c6f4f598469e4521c7fe2471d161814baf84
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-07 22:26:22 +0530

    Create Arithmetic_Operators.cpp

---
+++ Arithmetic_Operators.cpp
@@ -0,0 +1,27 @@
+# Arithmetic Operators 
+#----------Addition----------
+a = 10
+b = 20
+print(a+b) #30
+
+#---------Subtraction--------
+print(b-a) #10
+
+#---------Multiplication-----
+print(a*b) #200
+
+#---------Division-----------
+print(12/3) #4.0
+print(9/4) #2.25
+
+#--------Modulus-------------
+print(10%3) #1
+print(a%b) #10
+
+#--------Exponentiation------
+print(2**3) #8 (2^3 , base=2, power=3)
+print(4**1) #4
+
+#--------Floor Division------
+print(5//2) #2 (converted to floor value)
+print(11//3) #3
