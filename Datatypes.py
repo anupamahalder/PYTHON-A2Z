d4c1bccdcc045472c8c9c5442b73fba5071c7f89
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-07 19:16:13 +0530

    Update Datatypes.py

--- Datatypes.py
+++ Datatypes.py
@@ -1,21 +1,31 @@
-# string type data
+# -----------------string type data----------------------
 book = "Hello World"
 print(type(book))
-# number type data
+
+# -----------------number type data----------------------
 num1 = 45
 print(type(num1))
-# floating type
+
+# -----------------floating type-------------------------
 num2 = 34.9
 print(type(num2))
-# complex datatype 
+
+# ----------------complex datatype----------------------- 
 num = 42j
 print(num)
 print(type(num))
 
+# ----------------boolean datatype-----------------------
+Bool = True
+print(Bool)
+print(type(Bool))
+print(7==9) #False
 
 '''
   ========================OUTPUT SECTION==========================
   <class 'str'>
   <class 'int'>
   <class 'float'>
+  <class 'complex'>
+  <class 'bool'>
 '''
