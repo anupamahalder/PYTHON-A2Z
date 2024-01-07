fd912591377920ba441ae82ae4926df1f5c0b82c
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-07 19:39:50 +0530

    Create StringFormatting.cpp

---
+++ StringFormatting.cpp
@@ -0,0 +1,8 @@
+num1 = 20
+num2 = 50
+# string formatting  
+print(f"The added result of {num1} and {num2} is {num1+num2}") #This is the added result of 20 and 50 is 70
+
+userName = "Rupsha"
+presentClass = 8
+print(f"My name is {userName}, and I am in class {presentClass}") #My name is Rupsha, and I am in class 8
