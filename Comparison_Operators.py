c514a29eabb38898b8c9950e179d02df6a40cffb
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-08 23:31:55 +0530

    Create Comparison_Operators.py

---
+++ Comparison_Operators.py
@@ -0,0 +1,20 @@
+# Comparison Operator
+#-------------Equal------------------------
+a = 5
+b = 5
+print(a==b) #True
+
+#-------------Not Equal--------------------
+print(a!=b) #False
+
+#------------Greater than------------------
+print(a>b) #False
+
+#------------Less than---------------------
+print(a<b) #False
+
+#------------Greater than or equal to------
+print(a>=b) #True
+
+#------------Less than or equal to---------
+print(a<=b) #True
