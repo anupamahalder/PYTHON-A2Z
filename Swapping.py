e75b47979274f4fad488d33ca6f8e0f04c9ba921
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-11 23:55:24 +0530

    Update Swapping.py

--- Swapping.py
+++ Swapping.py
@@ -1,6 +1,11 @@
 a = 5
 b = 7
+print("Before swapping: ")
+print("The value of a = ",a)
+print("The value of b = ",b)
 #swapping
 a,b = b,a
 #after swapping
-print(a,b) #7 5
+print("Before swapping: ")
+print("The value of a = ",a) # 7
+print("The value of b = ",b) # 5
