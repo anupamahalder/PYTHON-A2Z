72442c31f7cca301ed734f604a1d212ccbc3f3b4
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-12 23:47:58 +0530

    Create 0011.UserInput.py

---
+++ 0011.UserInput.py
@@ -0,0 +1,12 @@
+name = input("Enter your name:")
+age = input("Enter your age:")
+
+print("Your name is ", name);
+print("Your age is ", age);
+
+'''
+Enter your name:Anupama Halder
+Enter your age:16
+Your name is  Anupama Halder
+Your age is  16
+'''
