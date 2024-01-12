05e11dfe89db8bd0b92972427e963567c9c7b58d
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-01-13 00:02:29 +0530

    Convert one data type to another data type

---
+++ 0012.Type_casting.py
@@ -0,0 +1,10 @@
+str = "1223"
+# ---------String to Integer conversion------------
+print(int(str)) #1223
+print(type(str)) #<class 'str'>
+
+# ---------String to float conversion-------------
+num = "122.3"
+print(float(num)) #122.3
+print(type(num)) #<class 'str'>
+
