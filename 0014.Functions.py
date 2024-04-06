d16f19d39ce67bcac84a5bd4142b8f391ae01f53
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-04-06 23:17:47 +0530

    Create 0014.Functions.py

---
+++ 0014.Functions.py
@@ -0,0 +1,10 @@
+# -------Function with single parameter---
+def double_it(num):
+    return 2*num
+# take user input 
+num = int(input("Enter a number:"))
+# call the function 
+res = double_it(num)
+print(res)
+
+# ------Function with multiple parameter---
