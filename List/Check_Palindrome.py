6df67320013b980952b1e5110a339840348f270f
Anupama Halder <110773499+anupamahalder@users.noreply.github.com>
2024-04-20 13:21:59 +0530

    Checking the palindrome of a string

---
+++ List/Check_Palindrome.py
@@ -0,0 +1,20 @@
+name = input("Enter a name: ")
+if(name == name[::-1]):
+    print("Yes, it is palindrome")
+else:
+    print("Not a palindrome")
+
+
+# --------Output section ---------
+"""
+Testcase 1:
+-----------
+Enter a name: madam
+Yes, it is palindrome
+-----------------------------
+
+Tescase 2:
+----------
+Enter a name: anupama
+Not a palindrome
+"""
